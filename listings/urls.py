6a804f28f3bfd42b86b97bc758dc176cf0ff5e06
sarorsaad <sarorsaad@gmail.com>
2022-08-26 03:17:05 +0300

    failed form

--- listings/urls.py
+++ listings/urls.py
@@ -6,8 +6,10 @@ app_name = 'listings'
 
 urlpatterns = [
     # Homepage
-    path(' ', views.all_listings, name='all_listings'),
+    path('', views.index, name='index'),
     # Listings Page
     path('all_listings/', views.all_listings, name='all_listings'),
+    # New Listing
+    path('new_listing/', views.new_listing, name='new_listing'),
    
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
-from .models import Listings
-
-
-def index(request):
-
-    return render(request, 'listings/index.html')
-
-def all_listings(request):
-
-    all_listings = Listings.objects.order_by('-list_date')
-
-    context = {'all_listings': all_listings}
-    return render(request, 'listings/all_listings.html', context)
+def new_listing(request):
+    if request.method != 'POST':
+        form = ListingForm()
+    else:
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listings:all_listings')
+      
+    context = {'form': form}
+    return render(request, 'listings/new_listing.html', context)
